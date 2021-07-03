c0a9cea6c2e9fd0ddc58b88fe6e4a5c0482ceef6
karim <k.guezlane@gmail.com>
2021-07-03 11:11:46 +0100

    fixed class= undifined bug

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,8 +1,12 @@
 const htmlCreate= function(htmlElement,id,_innerHtml,_class){
     let element= document.createElement(htmlElement);
 	element.setAttribute("id",id);
-    element.classList.add(_class);
 	element.innerHTML=_innerHtml;
+    if(!_class){
+        return element;
+    }else{
+        element.classList.add(_class);
+    }
 	return element;
 }
 const showHideElement= function(id,_class){
