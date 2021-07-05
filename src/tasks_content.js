f2ab479751dda037179dd5a5bb50ae0228d8869c
karim <k.guezlane@gmail.com>
2021-07-05 15:42:04 +0100

    cleaning code

--- src/tasks_content.js
+++ src/tasks_content.js
@@ -1,13 +1,11 @@
 import {showHideElement} from './domManipulation';
 import {htmlCreate} from './domManipulation';
 
-let tasks= [
+let tasks= [];
 
-];
 const deleteTask = function(e){
 	let j = e.target.dataset.index;
 	tasks.splice(j,1);
-	//localStorage.setItem("library",JSON.stringify(tasks));
 	renderTasks();
 };
 const showHideEditTaskPriority= function(e){
@@ -16,10 +14,9 @@ const showHideEditTaskPriority= function(e){
 }
 const editTaskPriority = function(e){
 	let j = e.target.dataset.index;
-	showHideEditTaskPriority(e);
 	if(e.target.localName !== "input"){return};
+	showHideEditTaskPriority(e);
 	tasks[j].priority = e.target.value;
-	console.log(tasks[j])
 }
 const priorityValue= function(){
 	let priorityValue
@@ -46,39 +43,48 @@ const pushTasks=function(e){
 		renderTasks();
 	}
 };
+const taskContent =function(i,task){
+	let taskContent =htmlCreate("div",`task${i}`,"","taskContent");
+	taskContent.innerHTML=`
+	<div class="taskDiv left" >
+	<input type="checkbox" class="checkTasks">
+	<h6>${task.name}</h6>
+	</div>
+	
+	<div class="taskDiv right">
+		<p>${task.date}</p>
+		<button id="edit${i}" data-index=${i}>Edit</button>
+		<button id="delete${i}" data-index=${i}>Delete</button>
+	</div>
+   `;
+   return taskContent;
+}
+const descriptionContent = function(i,task){
+	let descriptionContent = htmlCreate("div",`description${i}`,"","descriptionContent");
+	descriptionContent.innerHTML=` 
+	<p>${task.description}</p>
+	`;
+	return descriptionContent;
+}
+const editTaskPriorityForm = function(i){
+	let editTaskPriorityForm = htmlCreate("form",`editPriority${i}`,"","editForm" );
+	editTaskPriorityForm.innerHTML=`
+	<label for="priority">
+		<input type="radio" name="priority" class="edit_priority" data-index=${i} value="low"> Low 
+		<input type="radio" name="priority" class="edit_priority" data-index=${i} value="medium"> Medium
+		<input type="radio" name="priority" class="edit_priority" data-index=${i} value="high"> High 
+	</label>
+	`
+	return editTaskPriorityForm;
+}
 const renderTasks=function(){
 	let tasksContainer =document.querySelector("#tasksContainer");
 	tasksContainer.textContent="";
 	let i=0;
 	tasks.forEach((task)=>{
-		let taskContent =htmlCreate("div",`task${i}`,"","taskContent");
-		let descriptionContent = htmlCreate("div",`description${i}`,"","descriptionContent");
-		let editTaskPriorityForm = htmlCreate("form",`editPriority${i}`,"","editForm" );
-		taskContent.innerHTML=`
-		<div class="taskDiv left" >
-		<input type="checkbox" class="checkTasks">
-        <h6>${task.name}</h6>
-		</div>
-        
-		<div class="taskDiv right">
-			<p>${task.date}</p>
-			<button id="edit${i}" data-index=${i}>Edit</button>
-			<button id="delete${i}" data-index=${i}>Delete</button>
-		</div>
-       `;
-		descriptionContent.innerHTML=` 
-        <p>${task.description}</p>
-        `;
-		editTaskPriorityForm.innerHTML=`
-		<label for="priority">
-            <input type="radio" name="priority" class="edit_priority" data-index=${i} value="low"> Low 
-            <input type="radio" name="priority" class="edit_priority" data-index=${i} value="medium"> Medium
-            <input type="radio" name="priority" class="edit_priority" data-index=${i} value="high"> High 
-        </label>
-		`
-		tasksContainer.appendChild(taskContent);
-		tasksContainer.appendChild(descriptionContent);
-		tasksContainer.appendChild(editTaskPriorityForm);
+		tasksContainer.appendChild(taskContent(i,task));
+		tasksContainer.appendChild(descriptionContent(i,task));
+		tasksContainer.appendChild(editTaskPriorityForm(i));
 		document.getElementById(`editPriority${i}`).addEventListener("click",editTaskPriority);
 		document.getElementById(`edit${i}`).addEventListener("click", showHideEditTaskPriority);
 		document.getElementById(`delete${i}`).addEventListener("click", deleteTask)
