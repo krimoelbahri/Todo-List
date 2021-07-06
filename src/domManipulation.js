a7367b74cf53291ecbe685dce922f7a4cfa6594b
karim <k.guezlane@gmail.com>
2021-07-06 15:17:15 +0100

    finished my filterarray funtion

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,3 +1,6 @@
+import { formatISO } from 'date-fns'
+import { add } from 'date-fns'
+
 const htmlCreate= function(htmlElement,id,_innerHtml,_class){
     let element= document.createElement(htmlElement);
     if(!id){
@@ -16,5 +19,21 @@ const showHideElement= function(id,_class){
     let element=document.getElementById(id);
 	element.classList.toggle(_class);
 }
+const filterArray= function(arr,str){
+    if(str!== "today" && str!=="this week"){
+         let filtered= arr.filter((obj)=>(obj.project===str));
+         return filtered; 
+    }
+    if(str==="today"){
+        let filtered= arr.filter((obj)=>(obj.date===formatISO(new Date,{ representation: 'date' })));
+        return filtered; 
+    }
+    if(str==="this week"){
+        let filtered= arr.filter((obj)=>(obj.date >= formatISO(new Date,{ representation: 'date' }) 
+        && obj.date < formatISO(add(new Date,{days:7}),{ representation: 'date' })));
+        return filtered; 
+    }
+}
 export {htmlCreate};
 export {showHideElement};
+export {filterArray}
