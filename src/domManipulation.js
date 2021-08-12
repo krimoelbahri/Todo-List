2bc28b3683adc02b6218ec3b6d80297a2cedf9fe
karim <k.guezlane@gmail.com>
2021-08-12 08:19:39 +0100

    prettier format

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,39 +1,49 @@
-import { formatISO } from 'date-fns'
-import { add } from 'date-fns'
+import { formatISO } from "date-fns";
+import { add } from "date-fns";
 
-const htmlCreate= function(htmlElement,id,_innerHtml,_class){
-    let element= document.createElement(htmlElement);
-    if(!id){
-
-    }else{
-        element.setAttribute("id",id);
-    }
-	element.innerHTML=_innerHtml;
-    if(!_class){
-    }else{
-        element.classList.add(_class);
-    }
+const htmlCreate = function (htmlElement, id, _innerHtml, _class) {
+	let element = document.createElement(htmlElement);
+	if (!id) {
+	} else {
+		element.setAttribute("id", id);
+	}
+	element.innerHTML = _innerHtml;
+	if (!_class) {
+	} else {
+		element.classList.add(_class);
+	}
 	return element;
-}
-const showHideElement= function(id,_class){
-    let element=document.getElementById(id);
+};
+const showHideElement = function (id, _class) {
+	let element = document.getElementById(id);
 	element.classList.toggle(_class);
-}
-const filterArray= function(arr,str){
-    if(str!== "today" && str!=="this week"){
-         let filtered= arr.filter((obj)=>(obj.project===str));
-         return filtered; 
-    }
-    if(str==="today"){
-        let filtered= arr.filter((obj)=>(obj.date===formatISO(new Date,{ representation: 'date' })));
-        return filtered; 
-    }
-    if(str==="this week"){
-        let filtered= arr.filter((obj)=>(obj.date >= formatISO(new Date,{ representation: 'date' }) 
-        && obj.date < formatISO(add(new Date,{days:7}),{ representation: 'date' })));
-        return filtered; 
-    }
-}
-export {htmlCreate};
-export {showHideElement};
-export {filterArray}
+};
+const filterArray = function (arr, str) {
+	if (str !== "today" && str !== "this week") {
+		let filtered = arr.filter(
+			(obj) => obj.project.toLowerCase() === str
+		);
+		return filtered;
+	}
+	if (str === "today") {
+		let filtered = arr.filter(
+			(obj) =>
+				obj.date === formatISO(new Date(), { representation: "date" })
+		);
+		return filtered;
+	}
+	if (str === "this week") {
+		let filtered = arr.filter(
+			(obj) =>
+				obj.date >= formatISO(new Date(), { representation: "date" }) &&
+				obj.date <
+					formatISO(add(new Date(), { days: 7 }), {
+						representation: "date",
+					})
+		);
+		return filtered;
+	}
+};
+export { htmlCreate };
+export { showHideElement };
+export { filterArray };
