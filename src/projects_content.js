f74c522960f1d0448c33a8b5362b5e229f0be654
karim <k.guezlane@gmail.com>
2021-08-12 08:21:17 +0100

    integrating firebase into the project

--- src/projects_content.js
+++ src/projects_content.js
@@ -1,48 +1,67 @@
-import {showHideElement} from './domManipulation';
-import {htmlCreate} from './domManipulation';
-import{renderMain} from './tasks_content'
-let projects= [
-
-];
-const pushProject=function(){
-	showHideElement("addProjectForm","on");
-	if(document.getElementById("projectName").value === ""){
+import { showHideElement } from "./domManipulation";
+import { htmlCreate } from "./domManipulation";
+import { renderMain } from "./tasks_content";
+import { saveProject } from "./fireStore";
+const pushProject = function () {
+	showHideElement("addProjectForm", "on");
+	if (document.getElementById("projectName").value === "") {
 		return;
-	}else{
-		let projectName= {
-			name: document.getElementById("projectName").value ,
-		};
-		projects.push(projectName);
-		renderProjects();
+	} else {
+		let projectName = document.getElementById("projectName").value;
+		saveProject(projectName);
+		document.getElementById("projectName").value = "";
 	}
 };
-const addProjectSelect= function(){
-	let projectSelect= document.getElementById("projectSelect")
-	projectSelect.innerHTML ="" ;
-	projectSelect.innerHTML =`
-	<option value="">--Please choose an option--</option>
-	<option value="inbox">Default project</option>` ;	
-	for(let i=0; i<projects.length; i++){
-	projectSelect.innerHTML +=`<option value="${projects[i].name}">${projects[i].name}</option>` ;	
-	}
-}
 
-const projectsContent= function(){
-	let projectsContent =document.querySelector("#projectsContent");
-	projectsContent.textContent="";
-	let i=0;
-	projects.forEach((prj)=>{
-		let projectContainer = htmlCreate("div",`project${i}`,"","projectContainer")
-		projectContainer.innerHTML=`
-        <h6>${prj.name}</h6>
-        `;
-		projectsContent.appendChild(projectContainer);
-		projectContainer.addEventListener("click",renderMain)
-		i++;
+const displayProjectSelect = function (id, data) {
+	let projectSelect = document.getElementById("projectSelect");
+	if (document.getElementById(id)) {
+		return;
+	}
+	let projectOption = htmlCreate(
+		"option",
+		`${id}`,
+		`${data.name}`,
+		"projectOption"
+	);
+	projectSelect.appendChild(projectOption);
+};
+const displayProjects = function (id, data) {
+	let projectsContent = document.querySelector("#projectsContent");
+	if (document.getElementById(id)) {
+		return;
+	}
+	let projectContainer = htmlCreate(
+		"div",
+		`${id}`,
+		"",
+		"projectContainer"
+	);
+	projectContainer.innerHTML = `
+				<h6>${data.name}</h6>
+				`;
+	projectsContent.appendChild(projectContainer);
+	projectContainer.addEventListener("click", renderMain);
+};
+function loadProjects() {
+	// Create the query to load projects and listen for new ones.
+	var query = firebase
+		.firestore()
+		.collection("Projects")
+		.orderBy("timestamp", "asc");
+
+	// Start listening to the query.
+	query.onSnapshot(function (snapshot) {
+		snapshot.docChanges().forEach(function (change) {
+			if (change.type === "removed") {
+				console.log(change.doc.id);
+			} else {
+				var project = change.doc.data();
+				let id = change.doc.id;
+				displayProjects(id, project);
+				displayProjectSelect(`option${id}`, project);
+			}
+		});
 	});
 }
-const renderProjects=function(){
-	projectsContent();
-	addProjectSelect();
-};
-export{pushProject};
+export { pushProject, loadProjects };
--- src/tasks_content.js
+++ src/tasks_content.js
@@ -1,141 +1,238 @@
-import {showHideElement} from './domManipulation';
-import {htmlCreate} from './domManipulation';
-import {filterArray} from './domManipulation'
-let tasks= [];
-
-const deleteTask = function(e){
+import { showHideElement } from "./domManipulation";
+import { htmlCreate } from "./domManipulation";
+import { saveTasks } from "./fireStore";
+const deleteTask = function (e) {
 	let j = e.target.dataset.index;
-	tasks.splice(j,1);
-	renderTasks();
+	let query = firebase.firestore().collection("Tasks").doc(j);
+	query
+		.delete()
+		.then(() => {
+			tasksContainer.removeChild(document.getElementById(`task${j}`));
+			tasksContainer.removeChild(
+				document.getElementById(`description${j}`)
+			);
+			tasksContainer.removeChild(
+				document.getElementById(`editPriority${j}`)
+			);
+		})
+		.catch((error) => {
+			console.error("Error removing document: ", error);
+		});
 };
-const showHideDescription= function(e){
+const showHideDescription = function (e) {
 	let j = e.target.dataset.index;
-	showHideElement(`description${j}`,"on");
-}
+	showHideElement(`description${j}`, "on");
+};
 
-const showHideEditTaskPriority= function(e){
+const showHideEditTaskPriority = function (e) {
 	let j = e.target.dataset.index;
-	showHideElement(`editPriority${j}`,"on");
-}
-const setPriorityColor= function(elm,priority){
-	if(priority==="low"){
-		elm.style.borderLeft="10px solid green"
+	showHideElement(`editPriority${j}`, "on");
+};
+const setPriorityColor = function (elm, priority) {
+	if (priority === "low") {
+		elm.style.borderLeft = "10px solid green";
 	}
-	if(priority==="medium"){
-		elm.style.borderLeft="10px solid yellow"
+	if (priority === "medium") {
+		elm.style.borderLeft = "10px solid yellow";
 	}
-	if(priority==="high"){
-		elm.style.borderLeft="10px solid red"
+	if (priority === "high") {
+		elm.style.borderLeft = "10px solid red";
 	}
-}
-const editTaskPriority = function(e){
+};
+const editTaskPriority = function (e) {
 	let j = e.target.dataset.index;
-	if(e.target.localName !== "input"){return};
+	console.log(j);
+	if (e.target.localName !== "input") {
+		return;
+	}
 	showHideEditTaskPriority(e);
-	tasks[j].priority = e.target.value;
-	let taskToEdit= document.getElementById(`task${j}`)
-	setPriorityColor(taskToEdit,tasks[j].priority)
-}
-const priorityValue= function(){
-	let priorityValue
-	let priorities= (document.querySelectorAll(".priority"));
-		priorities.forEach((input)=>{
-			if(input.checked){
-				priorityValue= input.value;
-		}})
+	updatePriority(j, e.target.value);
+};
+const priorityValue = function () {
+	let priorityValue;
+	let priorities = document.querySelectorAll(".priority");
+	priorities.forEach((input) => {
+		if (input.checked) {
+			priorityValue = input.value;
+		}
+	});
 	return priorityValue;
 };
-const projectValue= function(){
+const projectValue = function () {
 	let projectValue = document.getElementById("projectSelect").value;
-	if(projectValue===""){
-		return("inbox");
-	}else{
-			return projectValue;
-		}
-};
-const checkTasks= function(e){
-	if(e.target.checked){
-		e.target.nextElementSibling.style.textDecoration="line-through"
-	}else{
-		e.target.nextElementSibling.style.textDecoration="none"
+	if (projectValue === "") {
+		return "inbox";
+	} else {
+		return projectValue;
 	}
-}
-const pushTasks=function(e){
-	e.preventDefault();
-	showHideElement("addTaskForm","on");
-	let task= {
-		name:document.getElementById("taskName").value,
-		description:document.getElementById("taskDescription").value,
-		date:document.getElementById("taskDueDate").value,
-		priority:priorityValue(),
-		project:projectValue(),
-	};
-	tasks.push(task);
-	renderTasks();
 };
-const taskContent =function(i,task){
-	let taskContent =htmlCreate("div",`task${i}`,"","taskContent");
-	taskContent.setAttribute("data-index",i);
-	setPriorityColor(taskContent,task.priority);
-	taskContent.innerHTML=`
+const checkTasks = function (e) {
+	if (e.target.checked) {
+		e.target.nextElementSibling.style.textDecoration = "line-through";
+	} else {
+		e.target.nextElementSibling.style.textDecoration = "none";
+	}
+};
+const pushTasks = function (e) {
+	e.preventDefault();
+	showHideElement("addTaskForm", "on");
+	let task = {
+		name: document.getElementById("taskName").value,
+		description: document.getElementById("taskDescription").value,
+		date: document.getElementById("taskDueDate").value,
+		priority: priorityValue(),
+		project: projectValue(),
+	};
+	saveTasks(
+		task.name,
+		task.description,
+		task.date,
+		task.priority,
+		task.project
+	);
+};
+
+const taskContent = function (id, task) {
+	let taskContent = htmlCreate("div", `task${id}`, "", "taskContent");
+	taskContent.setAttribute("data-index", `${id}`);
+	setPriorityColor(taskContent, task.priority);
+	taskContent.innerHTML = `
 	<div class="taskDiv left" >
 	<input type="checkbox" class="checkTasks">
-	<h6 id="taskName${i}" data-index=${i}>${task.name}</h6>
+	<h6 id="taskName${id}" data-index=${id}>${task.name}</h6>
 	</div>
 	
 	<div class="taskDiv right">
 		<p>${task.date}</p>
-		<button id="edit${i}" data-index=${i}>Edit</button>
-		<button id="delete${i}" data-index=${i}>Delete</button>
+		<button id="edit${id}" data-index=${id}>Edit</button>
+		<button id="delete${id}" data-index=${id}>Delete</button>
 	</div>
    `;
-   return taskContent;
-}
-const descriptionContent = function(i,task){
-	let descriptionContent = htmlCreate("div",`description${i}`,"","descriptionContent");
-	descriptionContent.innerHTML=` 
+	return taskContent;
+};
+
+const descriptionContent = function (id, task) {
+	let descriptionContent = htmlCreate(
+		"div",
+		`description${id}`,
+		"",
+		"descriptionContent"
+	);
+	descriptionContent.innerHTML = ` 
 	<p>${task.description}</p>
 	`;
 	return descriptionContent;
-}
-const editTaskPriorityForm = function(i){
-	let editTaskPriorityForm = htmlCreate("form",`editPriority${i}`,"","editForm" );
-	editTaskPriorityForm.innerHTML=`
+};
+
+const editTaskPriorityForm = function (id) {
+	let editTaskPriorityForm = htmlCreate(
+		"form",
+		`editPriority${id}`,
+		"",
+		"editForm"
+	);
+	editTaskPriorityForm.innerHTML = `
 	<label for="priority">
-		<input type="radio" name="priority" class="edit_priority" data-index=${i} value="low"> Low 
-		<input type="radio" name="priority" class="edit_priority" data-index=${i} value="medium"> Medium
-		<input type="radio" name="priority" class="edit_priority" data-index=${i} value="high"> High 
+		<input type="radio" name="priority" class="edit_priority" data-index=${id} value="low"> Low 
+		<input type="radio" name="priority" class="edit_priority" data-index=${id} value="medium"> Medium
+		<input type="radio" name="priority" class="edit_priority" data-index=${id} value="high"> High 
 	</label>
-	`
+	`;
 	return editTaskPriorityForm;
-}
-const renderMain= function(e){
-	document.getElementById("projectTitle").innerHTML=e.target.innerText;
-	renderTasks();
-}
-const renderTasks=function(){
-	let project= document.getElementById("projectTitle").innerHTML;
-	let tasksContainer =document.querySelector("#tasksContainer");
-	tasksContainer.textContent="";
-	let i=0;
-	let tasksArray=tasks;
-	let str=project.toLocaleLowerCase();
-	if( str !=="inbox"){
-		tasksArray= filterArray(tasks,str)
+};
+const renderMain = function (e) {
+	document.getElementById("projectTitle").innerHTML =
+		e.target.innerText;
+	loadFilteredTasks(e.target.innerText);
+};
+const displayTasks = function (id, data) {
+	//let project = document.getElementById("projectTitle").innerHTML;
+	let tasksContainer = document.querySelector("#tasksContainer");
+	if (document.getElementById(`task${id}`)) {
+		return;
 	}
-	tasksArray.forEach((task)=>{
-		tasksContainer.appendChild(editTaskPriorityForm(i));
-		tasksContainer.appendChild(taskContent(i,task));
-		tasksContainer.appendChild(descriptionContent(i,task));
-		document.getElementById(`editPriority${i}`).addEventListener("click",editTaskPriority);
-		document.getElementById(`edit${i}`).addEventListener("click", showHideEditTaskPriority);
-		document.getElementById(`delete${i}`).addEventListener("click", deleteTask)
-		document.getElementById(`taskName${i}`).addEventListener("click",showHideDescription)
-		document.querySelectorAll('.checkTasks').forEach((check)=>{
-			check.addEventListener("click",checkTasks);
-		})
-		i++;
+	tasksContainer.appendChild(editTaskPriorityForm(id));
+	tasksContainer.appendChild(taskContent(id, data));
+	tasksContainer.appendChild(descriptionContent(id, data));
+	document
+		.getElementById(`editPriority${id}`)
+		.addEventListener("click", editTaskPriority);
+	document
+		.getElementById(`edit${id}`)
+		.addEventListener("click", showHideEditTaskPriority);
+	document
+		.getElementById(`delete${id}`)
+		.addEventListener("click", deleteTask);
+	document
+		.getElementById(`taskName${id}`)
+		.addEventListener("click", showHideDescription);
+	document.querySelectorAll(".checkTasks").forEach((check) => {
+		check.addEventListener("click", checkTasks);
 	});
 };
-export{pushTasks};
-export{renderMain};
+
+function loadTasks() {
+	// Create the query to load projects and listen for new ones.
+	let query = firebase
+		.firestore()
+		.collection("Tasks")
+		.orderBy("timestamp", "asc");
+
+	// Start listening to the query.
+	query.onSnapshot(function (snapshot) {
+		snapshot.docChanges().forEach(function (change) {
+			if (change.type === "removed") {
+			} else {
+				let project = change.doc.data();
+				let id = change.doc.id;
+				displayTasks(id, project);
+			}
+		});
+	});
+}
+function loadFilteredTasks(filter) {
+	let tasksContainer = document.querySelector("#tasksContainer");
+	tasksContainer.innerHTML = "";
+	let query = firebase.firestore().collection("Tasks");
+
+	query = query
+		.where("project", "==", filter)
+		.orderBy("timestamp", "asc");
+	console.log(query);
+
+	// Start listening to the query.
+	query.onSnapshot(function (snapshot) {
+		snapshot.docChanges().forEach(function (change) {
+			if (change.type === "removed") {
+			} else {
+				let project = change.doc.data();
+				let id = change.doc.id;
+				displayTasks(id, project);
+			}
+		});
+	});
+}
+function updatePriority(id, value) {
+	let db = firebase.firestore();
+	var docToUpdate = db.collection("Tasks").doc(id);
+	return db
+		.runTransaction((transaction) => {
+			return transaction.get(docToUpdate).then((document) => {
+				if (!document.exists) {
+					throw "Document does not exist!";
+				}
+				var newPriority = value;
+				transaction.update(docToUpdate, { priority: newPriority });
+			});
+		})
+		.then(() => {
+			docToUpdate.get().then((result) => {
+				let taskToEdit = document.getElementById(`task${id}`);
+				setPriorityColor(taskToEdit, result.data().priority);
+			});
+		})
+		.catch((error) => {
+			console.error("Transaction failed: ", error);
+		});
+}
+export { pushTasks, renderMain, loadTasks };
